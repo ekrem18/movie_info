a148d8e1654444b9351c9226833be44482677cf2
Ekrem <avekremyilmazturk@gmail.com>
2023-08-05 11:55:02 +0300

    buttons and css

--- src/components/FlimYear.jsx
+++ src/components/FlimYear.jsx
@@ -6,7 +6,7 @@ const FlimYear = ({ data, setInfo }) => {
         <div className="d-flex flex-column" >
             {
                 data.map(item=> 
-                <button onClick={()=>setInfo(item.date)} type="button" className=" border-4   bg-info mb-2 p-1 rounded-2 fs-4 fw-bold text-secondary border-bottom border-warning " key={item.id}>
+                <button onClick={()=>setInfo(item.date)} type="button" className=" btn mt-2 " key={item.id}>
                 {item.date}
                 </button> )
             }
