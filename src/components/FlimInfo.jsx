4f3c77b78dd128d5bca381a8aa7a95e1aaa7db85
Ekrem <avekremyilmazturk@gmail.com>
2023-08-07 13:09:46 +0300

    editing

--- src/components/FlimInfo.jsx
+++ src/components/FlimInfo.jsx
@@ -5,24 +5,14 @@ const FlimInfo = ({ data, info }) => {
 
     const filteredData = data.filter(item => item.date === info)
     const dataObj = filteredData[0]
-    /*    let start = ""
-       for (let i = 1; i < 6; i++) {
-   
-   
-        
-   
-           if(i <= dataObj.rate/2){
-               start += "⭐"
-           }
-   
-       } */
+  
 
     return (
         <div className=" right_card pt-3 w-50 d-flex">
             <div>
                 <h1 className="text-primary">{dataObj?.title}</h1>
                 <p>{dataObj?.body}</p>
-                <p className="text-danger">{dataObj?.tags.join(" $ ")}</p>
+                <p className="text-danger">{dataObj?.tags.join(" / ")}</p>
                 <p>
                     <span>{dataObj.rate < 6 ? "⭐⭐⭐" : dataObj.rate < 8 ? "⭐⭐⭐⭐" : dataObj.rate > 8 ? "⭐⭐⭐⭐⭐" : ""} </span>
                     <span className="text-success">{dataObj?.rate.toFixed(1)}/10</span>
